70848c9a4c5e892f67d27b2c68a3aa87396ba517
Anchit Jain <anchitjain1234@gmail.com>
2015-04-05 02:06:55 +0530

    some changes according to problem asked to make title+snippet each for google and yahoo

--- read1.py
+++ read1.py
@@ -5,13 +5,18 @@ def read_raw_json(inputfile,outputfile):
     path='/home/anchit/py/irproject2/googleresults/googleresults/'+inputfile
     with open(path) as data_file:    
         data = json.load(data_file)
-    f=open(outputfile,'w')
+    f=open(outputfile+"urls.txt",'w')
     for i in range(len(data)):
-        f.write(data[i]["link"]+"\n") 
+        f.write(data[i]["link"]+"\n")
+    f.close()
+    f=open(outputfile+"titleandsnippet.txt","w")
+    for i in range(len(data)):
+        string=outputfile+str(i+1)
+        f.write(string.encode('utf8')+"    :"+data[i]["title"].encode('utf8')+" "+data[i]["desc"].encode('utf8')+"\n\n\n")      
     f.close()
 
-read_raw_json("t2.json","googleresultsordered.txt")
-read_raw_json("items2.json","yahooresultsordered.txt")
+read_raw_json("t2.json","google")
+read_raw_json("items2.json","yahoo")
 
 
 
