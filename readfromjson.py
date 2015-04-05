e7b1e6b8af64823316ec97fd27c910765e8e5d3d
Anchit Jain <anchitjain1234@gmail.com>
2015-04-05 10:48:53 +0530

    SOme changes

---
+++ readfromjson.py
@@ -0,0 +1,111 @@
+import json
+from pprint import pprint
+urls=[]
+
+def algo1(urls,glink,ylink,commonlinks):
+    algo1res=[0 for i in range(len(urls))]
+    temp=[]
+    for url in commonlinks:
+        in1=glink.index(url)
+        in2=ylink.index(url)
+        
+        pos=min(in1,in2)
+        """
+        algo1res[pos]=url
+        """
+        temp.append([pos,url])
+    temp=sorted(temp,key=lambda l:l[0])
+    for i in range(len(commonlinks)):
+        algo1res[i]=temp[i][1]
+    for url in urls:
+        if(url not in algo1res):
+            if url in glink:
+                pos=glink.index(url)
+            else:
+                pos=ylink.index(url)
+            while(algo1res[pos]!=0):
+                pos+=1
+            algo1res[pos]=url
+        
+    
+    f=open("ResultantRanks_A1.txt","w")
+    for url in algo1res:
+        f.write(str(url)+"\n")
+    return algo1res
+
+def algo2(urls,glink,ylink,commonlinks):
+    algo2res=[0 for i in range(len(urls))]
+    temp=[]
+    for url in urls:
+        if(url in glink):
+            in1=glink.index(url)
+        else:
+            in1=0
+        
+        if(url in ylink):
+            in2=ylink.index(url)
+        else:
+            in2=0
+        pos=in1+in2
+        """
+        algo1res[pos]=url
+        """
+        temp.append([pos,url])
+    temp=sorted(temp,key=lambda l:l[0])
+    for i in range(len(urls)):
+        algo2res[i]=temp[i][1]
+    
+
+
+    f=open("ResultantRanks_A2.txt","w")
+    for url in algo2res:
+        f.write(str(url)+"\n")
+    return algo2res  
+    
+def printagg(commonlinks):
+    f=open("aggregateddata.txt","w")
+    for i in range(len(gdata)):
+        f.write(gtitsnippet[i])
+        urls.append(glink[i])
+    for i in range(len(ydata)):
+        if ylink[i] not in commonlinks:
+            f.write(ytitsnippet[i])
+            urls.append(ylink[i])
+    f.close()
+    
+def read_raw_json(inputfile,outputfile):
+    path='/home/anchit/py/irproject2/googleresults/googleresults/'+inputfile
+    with open(path) as data_file:    
+        data = json.load(data_file)
+    f=open(outputfile+"urls.txt",'w')
+    for i in range(len(data)):
+        f.write(data[i]["link"]+"\n")
+    f.close()
+    f=open(outputfile+"titleandsnippet.txt","w")
+    for i in range(len(data)):
+        string=outputfile+str(i+1)
+        data[i]["docid"]=string
+        f.write(string.encode('utf8')+"    :"+data[i]["title"].encode('utf8')+" "+data[i]["desc"].encode('utf8')+"\n\n\n")      
+    f.close()
+    return data
+
+gdata=read_raw_json("t2.json","google")
+ydata=read_raw_json("items2.json","yahoo")
+
+glink=[]
+ylink=[]
+gtitsnippet=[]
+ytitsnippet=[]
+for i in range(len(gdata)):
+    glink.append(gdata[i]["link"])
+    gtitsnippet.append(gdata[i]["docid"]+"    :"+gdata[i]["title"].encode('utf8')+" "+gdata[i]["desc"].encode('utf8')+"\n\n\n")    
+
+for i in range(len(ydata)):
+    ylink.append(ydata[i]["link"])
+    ytitsnippet.append(ydata[i]["docid"]+"    :"+ydata[i]["title"].encode('utf8')+" "+ydata[i]["desc"].encode('utf8')+"\n\n\n")
+
+commonlinks= set(glink).intersection(ylink)
+printagg(commonlinks)
+
+algo1res=algo1(urls, glink, ylink,commonlinks)
+algo2res=algo2(urls, glink, ylink,commonlinks)
