509c62406022f7e358cc27d65a0cbe912426cd9e
Anchit Jain <anchitjain1234@gmail.com>
2015-04-05 16:02:46 +0530

    readfromjson.py modified to correct the final text to be printed in correct encoding.

--- readfromjson.py
+++ readfromjson.py
@@ -1,6 +1,19 @@
 import json
 from pprint import pprint
+import html2text
+from bs4 import BeautifulSoup
+#import matplotlib.pyplot as plt
 urls=[]
+
+def calcprec(links,manuallinks,prec):
+    ct=0
+    for i in range(prec):
+        t1=links[i].encode('utf-8')
+        t2=manuallinks[i].rstrip()
+        if(t1==t2):
+            ct+=1
+    t=ct/float(prec)
+    return t
 
 def algo1(urls,glink,ylink,commonlinks):
     algo1res=[0 for i in range(len(urls))]
@@ -26,7 +39,6 @@ def algo1(urls,glink,ylink,commonlinks):
             while(algo1res[pos]!=0):
                 pos+=1
             algo1res[pos]=url
-        
     
     f=open("ResultantRanks_A1.txt","w")
     for url in algo1res:
@@ -69,7 +81,7 @@ def printagg(commonlinks):
         urls.append(glink[i])
     for i in range(len(ydata)):
         if ylink[i] not in commonlinks:
-            f.write(ytitsnippet[i])
+            f.write(ytitsnippet[i].encode('utf-8'))
             urls.append(ylink[i])
     f.close()
     
@@ -85,6 +97,9 @@ def read_raw_json(inputfile,outputfile):
     for i in range(len(data)):
         string=outputfile+str(i+1)
         data[i]["docid"]=string
+        data[i]["link"]=data[i]["link"].encode('utf8')
+        data[i]["desc"]=BeautifulSoup(data[i]["desc"]).get_text().strip()
+        data[i]["title"]=BeautifulSoup(data[i]["title"].encode('utf8')).get_text().strip()
         f.write(string.encode('utf8')+"    :"+data[i]["title"].encode('utf8')+" "+data[i]["desc"].encode('utf8')+"\n\n\n")      
     f.close()
     return data
@@ -98,15 +113,31 @@ gtitsnippet=[]
 ytitsnippet=[]
 for i in range(len(gdata)):
     glink.append(gdata[i]["link"])
-    gtitsnippet.append(gdata[i]["docid"]+"    :"+gdata[i]["title"].encode('utf8')+" "+gdata[i]["desc"].encode('utf8')+"\n\n\n")    
+    gtitsnippet.append(gdata[i]["docid"]+"    :"+gdata[i]["title"].encode('utf-8')+" "+gdata[i]["desc"].encode('utf-8')+"\n\n\n")    
 
 for i in range(len(ydata)):
-    ylink.append(ydata[i]["link"])
-    ytitsnippet.append(ydata[i]["docid"]+"    :"+ydata[i]["title"].encode('utf8')+" "+ydata[i]["desc"].encode('utf8')+"\n\n\n")
+    ylink.append(ydata[i]["link"].encode('utf8'))
+    ydata[i]["title"]=ydata[i]["title"].encode('utf8')
+    ydata[i]["desc"]=ydata[i]["desc"].encode('utf8')    
+    ytitsnippet.append(ydata[i]["docid"]+"    :"+ydata[i]["title"].decode('utf-8')+" "+ydata[i]["desc"].decode('utf-8')+"\n\n\n")
 
 commonlinks= set(glink).intersection(ylink)
 printagg(commonlinks)
-print commonlinks
-print len(commonlinks)
+
 algo1res=algo1(urls, glink, ylink,commonlinks)
 algo2res=algo2(urls, glink, ylink,commonlinks)
+
+gprec=[]
+yprec=[]
+
+with open("RankedDocuments.txt") as f:
+    manuallinks=f.readlines()
+for i in range(6):
+    t=calcprec(algo1res,manuallinks,5*(i+1))
+    gprec.append(t)
+    t=calcprec(algo2res, manuallinks,5*(i+1))
+    yprec.append(t)
+    
+print gprec
+print yprec
+#plt.plot(gprec,gprec)
